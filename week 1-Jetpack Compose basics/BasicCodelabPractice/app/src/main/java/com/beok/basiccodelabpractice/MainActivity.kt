0056f7480ac70ddb5dcdae8454ba03f4bb5eea31
강현석 <kekemusa37@gmail.com>
2021-11-03 14:02:17 +0900

    [Jetpack Compose basics] 09. Creating a performant lazy list

--- week 1-Jetpack Compose basics/BasicCodelabPractice/app/src/main/java/com/beok/basiccodelabpractice/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicCodelabPractice/app/src/main/java/com/beok/basiccodelabpractice/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.beok.basiccodelabpractice
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
@@ -49,9 +51,10 @@ fun MyApp() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+private fun Greetings(names: List<String> = List(1_000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Log.d("ryan", "LazyColumn $name is called!")
             Greeting(name = name)
         }
     }
