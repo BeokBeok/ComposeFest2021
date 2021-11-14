dc745f510199f161cfcf5c87d78c223cbb45e59f
강현석 <kekemusa37@gmail.com>
2021-11-14 17:20:52 +0900

    [Layouts in Jetpack Compose] 07. Create your custom layout | Using the layout modifier

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/beok/layouts/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/beok/layouts/MainActivity.kt
@@ -34,8 +34,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.beok.layouts.ui.theme.LayoutsTheme
@@ -159,6 +163,39 @@ fun ImageListItem(index: Int) {
             text = "Item #$index",
             style = MaterialTheme.typography.subtitle1
         )
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsTheme {
+        Text(text = "Hi there!", modifier = Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsTheme {
+        Text(text = "Hi there!", modifier = Modifier.padding(top = 32.dp))
     }
 }
 
