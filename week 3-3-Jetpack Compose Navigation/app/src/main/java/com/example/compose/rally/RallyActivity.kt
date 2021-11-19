16c28593ccfcca1d5d4367d3fdf5edd93633459e
강현석 <kekemusa37@gmail.com>
2021-11-19 12:05:25 +0900

    [Jetpack Compose Navigation] 07. Testing Navigation in Compose

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -81,9 +81,9 @@ fun RallyApp() {
 }
 
 @Composable
-private fun RallyNavHost(
+fun RallyNavHost(
     navController: NavHostController,
-    modifier: Modifier
+    modifier: Modifier = Modifier
 ) {
     NavHost(
         navController = navController,
